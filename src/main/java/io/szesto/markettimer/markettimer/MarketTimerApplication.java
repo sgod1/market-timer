2836e73abad6bc73afce1702f5ab7a3eb42037e6
Simon Godik <simon@godik.com>
2026-06-05 16:33:11 -0700

    maven compiler plugin

--- src/main/java/io/szesto/markettimer/markettimer/MarketTimerApplication.java
+++ src/main/java/io/szesto/markettimer/markettimer/MarketTimerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MarketTimerApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(MarketTimerApplication.class, args);
     }
 
