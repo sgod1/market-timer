d6cfac4231426c360079fd11a92aff9eca630b56
Simon Godik <simon@godik.com>
2026-06-05 12:52:46 -0700

    empty commit

---
+++ src/main/java/io/szesto/markettimer/markettimer/MarketTimerApplication.java
@@ -0,0 +1,13 @@
+package io.szesto.markettimer.markettimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketTimerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketTimerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/szesto/markettimer/markettimer/MarketTimerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.szesto.markettimer.markettimer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketTimerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
